4b7748592412d26bca88261b550572192851811d
Abc <abc@Abcs-MacBook-Pro.local>
2022-12-19 11:18:42 +0800

    Initial commit

--- enigma-machine/python-version/keyboard.py
+++
@@ -1,9 +0,0 @@
-#keyboard
-class Keyboard:
-    
-    def forward(self, letter):
-        signal = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".find(letter)
-        return signal
-    def backward(self, signal):
-        letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[signal]
-        return letter
