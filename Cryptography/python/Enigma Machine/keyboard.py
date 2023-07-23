f12f60f9b5d841be6b62b5e325bd26cbf0c4cc82
Arjundevjha <111162691+Arjundevjha@users.noreply.github.com>
2023-07-23 05:38:22 +0000

    "Enigma"

---
+++ Cryptography/python/Enigma Machine/keyboard.py
@@ -0,0 +1,11 @@
+class Keyboard:
+    def forward(self, letter):
+        signal = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".find(letter)
+        return signal
+    
+    def backward(self,signal):
+        letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[signal]
+        return letter
+    
+k = Keyboard()
+print(k.forward("A"))
