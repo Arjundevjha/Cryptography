99992ff574066d76d87dd3d3028dc1dad9f98e90
Arjundevjha <111162691+Arjundevjha@users.noreply.github.com>
2025-07-27 16:16:24 +0800

    hashing

---
+++ src/python/historical/enigma/__init__.py
@@ -0,0 +1,8 @@
+#init file for enigma module
+
+from .enigma import Enigma
+from .keyboard import Keyboard
+from .rotor import Rotor
+from .plugboard import Plugboard
+from .reflector import Reflector
+
--- src/python/modern/hash_functions.py
+++ src/python/modern/hash_functions.py
@@ -1,16 +1,89 @@
-def hash(string):
-  # the prime number used in the hash function
-  PRIME = 31
+#class for hash functions
+import hashlib
 
-  # initialize the hash value to the length of the string
-  hash_value = len(string)
+class HashFunction:
+    def __init__(self, name, hash_function):
+        self.name = name
+        self.hash_function = hash_function
 
-  # add the ASCII value of each character in the string to the hash value
-  for char in string:
-    hash_value += ord(char)
+    def main(self, data):
+        """
+        Applies the hash function to the given data.
+        
+        :param data: The input data to hash.
+        :return: The hashed value.
+        """
+        return self.hash_function(data)
+      
+    def sha256_hash(self, data):
+        """
+        Computes the SHA-256 hash of the given data.
+        
+        :param data: The input data to hash.
+        :return: The SHA-256 hash of the data.
+        """
+        return hashlib.sha256(data.encode()).hexdigest()
+      
+    def md5_hash(self, data):
+        """
+        Computes the MD5 hash of the given data.
+        
+        :param data: The input data to hash.
+        :return: The MD5 hash of the data.
+        """
+        return hashlib.md5(data.encode()).hexdigest()
+      
+    def sha1_hash(self, data):
+        """
+        Computes the SHA-1 hash of the given data.
+        
+        :param data: The input data to hash.
+        :return: The SHA-1 hash of the data.
+        """
+        return hashlib.sha1(data.encode()).hexdigest()
+      
+    def sha512_hash(self, data):
+        """
+        Computes the SHA-512 hash of the given data.
+        
+        :param data: The input data to hash.
+        :return: The SHA-512 hash of the data.
+        """
+        return hashlib.sha512(data.encode()).hexdigest()
+      
+    def sha3_256_hash(self, data):
+        """
+        Computes the SHA3-256 hash of the given data.
+        
+        :param data: The input data to hash.
+        :return: The SHA3-256 hash of the data.
+        """
+        return hashlib.sha3_256(data.encode()).hexdigest()
+      
+    def blake2b_hash(self, data):
+      
+        """
+        Computes the BLAKE2b hash of the given data.
+        
+        :param data: The input data to hash.
+        :return: The BLAKE2b hash of the data.
+        """
+        return hashlib.blake2b(data.encode()).hexdigest()
+      
+    def blake2s_hash(self, data):
+        """
+        Computes the BLAKE2s hash of the given data.
+        
+        :param data: The input data to hash.
+        :return: The BLAKE2s hash of the data.
+        """
+        return hashlib.blake2s(data.encode()).hexdigest()
+      
+      
+if __name__ == "__main__":
 
-  # multiply the hash value by the prime number and return it
-  return hash_value * PRIME
+    data = input("Enter data to hash: ")
+    hash_algorithm = input("Enter hash algorithm (sha256, md5, sha1, sha512, sha3_256, blake2b, blake2s): ")
 
-hashed_string = hash("hello")
-# hashed_string will be 271
+    hash_function = HashFunction(hash_algorithm, getattr(HashFunction, f"{hash_algorithm}_hash"))
+    print(f"{hash_algorithm.upper()} Hash: {hash_function.main(data)}")
