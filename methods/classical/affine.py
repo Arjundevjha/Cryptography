5dc3950d6a3c29a981538ed62ec5047838b7a608
Arjundevjha <111162691+Arjundevjha@users.noreply.github.com>
2026-01-25 14:54:53 +0800

    feat: Implement Affine cipher with encryption and decryption functions

---
+++ methods/classical/affine.py
@@ -0,0 +1,71 @@
+import random
+import typing
+
+letters = {
+    'a': 0,
+    'b': 1,
+    'c': 2,
+    'd': 3,
+    'e': 4,
+    'f': 5,
+    'g': 6,
+    'h': 7,
+    'i': 8,
+    'j': 9,
+    'k': 10,
+    'l': 11,
+    'm': 12,
+    'n': 13,
+    'o': 14,
+    'p': 15,
+    'q': 16,
+    'r': 17,
+    's': 18,
+    't': 19,
+    'u': 20,
+    'v': 21,
+    'w': 22,
+    'x': 23,
+    'y': 24,
+    'z': 25
+}
+
+
+coprimes = [1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61, 63, 67, 69, 71, 73, 75, 77, 79, 81, 83, 85, 87, 89, 93, 95, 97, 99, 101, 103, 105, 107, 109, 111, 113, 115, 119, 121, 123, 125, 127, 129, 131, 133, 135, 137, 139, 141, 145, 147, 149, 151, 153, 155, 157, 159, 161, 163, 165, 167, 171, 173, 175, 177, 179, 181, 183, 185, 187, 189, 191, 193, 197, 199, 201, 203, 205, 207, 209, 211, 213, 215]
+
+a_key = random.choice(coprimes)
+coprimes.remove(a_key)
+b_key = random.choice(coprimes)
+
+def encrypt(plaintext: str, a_key: int, b_key: int):
+    ciphertext = ""
+    for char in plaintext:
+        if char.isalpha():
+            char = char.lower()
+            char_index = letters[char]
+            encrypted_char_index = (a_key * char_index + b_key) % 26
+            ciphertext += list(letters.keys())[list(letters.values()).index(encrypted_char_index)]
+        else:
+            ciphertext += char
+    return ciphertext
+
+def decrypt(ciphertext: str, a_key: int, b_key: int):
+    decrypted_text = ""
+    for char in ciphertext:
+        if char.isalpha():
+            char = char.lower()
+            char_index = letters[char]
+            a_inverse = pow(a_key, -1, 26)
+            decrypted_char_index = (a_inverse * (char_index - b_key)) % 26
+            decrypted_text += list(letters.keys())[list(letters.values()).index(decrypted_char_index)]
+        else:
+            decrypted_text += char
+    return decrypted_text
+
+if __name__ == "__main__":
+    message = input("Please enter a message: ")
+    encrypted = encrypt(message, a_key, b_key)
+    decrypted = decrypt(encrypted, a_key, b_key)
+    print(f"Original: {message}")
+    print(f"Encrypted: {encrypted}")
+    print(f"Decrypted: {decrypted}")
